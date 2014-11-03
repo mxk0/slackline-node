ca8fdd8caea8716815720d9c8e212d21228f428e
Max Kramer <max@fogcreek.com>
2014-11-02 20:58:04 -0800

    Added a template settings.js file

---
+++ settings.js
@@ -0,0 +1,10 @@
+var settings = {};
+
+settings.tokens = {
+	'[FIRST SLACK INSTANCE]' : 'FIRST API KEY',
+	'[SECOND SLACK INSTANCE]' : 'SECOND API KEY'
+};
+
+settings.postUrl = '/services/hooks/incoming-webhook?token=';
+
+module.exports = settings;
